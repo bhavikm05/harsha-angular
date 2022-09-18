8ff234fd284cfbab6055bc0584bbfbd7ab80f28c
Bhavik Shah <bhavik.shah@ilink-systems.com>
2022-09-18 09:57:16 -0400

    Project Created and basic dashboard setup

--- 01. Creating First App in Angular/TaskManager/src/app/app-routing.module.ts
+++ 01. Creating First App in Angular/TaskManager/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "about", component: AboutComponent},
+  {path: "", redirectTo: "dashboard" , pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+
+}
